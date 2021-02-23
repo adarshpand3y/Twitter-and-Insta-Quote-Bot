a555455d184b6b0c5ed71588dda0d65ec1f6bcfd
unknown <Adarsh Pandey>
2021-02-23 15:24:41 +0530

    Added authorization,quote fetching function and added environment variables

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
+import tweepy
 import time
-a = 0
-while a<5:
-    print(a)
-    a += 1
-    time.sleep(3)
-print("Program ended successfully")
+from requests import get
+from json import loads
+import os
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+
+SLEEP_DURATION = environ['SLEEP_DURATION']
+HASHTAG1 = environ['HASHTAG1']
+HASHTAG2 = environ['HASHTAG2']
+HASHTAG3 = environ['HASHTAG3']
+
+# A function to get a quote
+def getquote():
+    try:
+        response = get('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en')
+        q = '{quoteText} - {quoteAuthor}\n'.format(**loads(response.text))
+    except:
+        return "0"
+    
+    return q
+
+# Main program starts here
+if __name__ == "__main__":
+    
+    # Authorizing twitter account
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+
+    # Printing the quotes and waiting for one hour
+    while True:
+        quote = getquote()
+        if quote != "0":
+            final_quote = f"{quote}\n{HASHTAG1} {HASHTAG2} {HASHTAG3}"
+            api.update_status(final_quote)
+        time.sleep(3600)
